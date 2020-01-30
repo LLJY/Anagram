c291a544fd8d8d98d3a299c25ddf2ca8050847f4
Lucas Lee <lucasleeeeeeeee@gmail.com>
2020-01-30 23:18:08 +0800

    MainViewModel: move calculating word value to it's own function
    this function will be called more than once

--- app/src/main/java/com/lucas/anagram/MainViewModel.kt
+++ app/src/main/java/com/lucas/anagram/MainViewModel.kt
@@ -42,38 +42,7 @@ class MainViewModel : ViewModel() {
             try {
                 val input = BufferedReader(InputStreamReader(url.openStream()))
                 input.lines().forEach{
-                    var value = 0L
-                    it.forEach {
-                        when(it){
-                            'A' -> value += 2
-                            'B' -> value += 3
-                            'C' -> value += 5
-                            'D' -> value += 7
-                            'E' -> value += 11
-                            'F' -> value += 13
-                            'G' -> value += 17
-                            'H' -> value += 19
-                            'I' -> value += 23
-                            'J' -> value += 29
-                            'K' -> value += 31
-                            'L' -> value += 37
-                            'M' -> value += 41
-                            'N' -> value += 43
-                            'O' -> value += 47
-                            'P' -> value += 53
-                            'Q' -> value += 59
-                            'R' -> value += 61
-                            'S' -> value += 67
-                            'T' -> value += 71
-                            'U' -> value += 73
-                            'V' -> value += 79
-                            'W' -> value += 83
-                            'X' -> value += 89
-                            'Y' -> value += 97
-                            'Z' -> value += 101
-                        }
-                    }
-                    returnlist.add(WordData(it, value))
+                    returnlist.add(WordData(it, GetStringValue(it)))
                 }
                 success = true
             } catch (e: IOException) {
@@ -82,6 +51,41 @@ class MainViewModel : ViewModel() {
             }
         }
         WordsList = returnlist
+    }
+    //calculate the value in an inline function instead as this long ass function will be used more than once
+    inline fun GetStringValue(word: String): Long{
+        var value = 0L
+        word.forEach {
+            when(it){
+                'A' -> value += 2
+                'B' -> value += 3
+                'C' -> value += 5
+                'D' -> value += 7
+                'E' -> value += 11
+                'F' -> value += 13
+                'G' -> value += 17
+                'H' -> value += 19
+                'I' -> value += 23
+                'J' -> value += 29
+                'K' -> value += 31
+                'L' -> value += 37
+                'M' -> value += 41
+                'N' -> value += 43
+                'O' -> value += 47
+                'P' -> value += 53
+                'Q' -> value += 59
+                'R' -> value += 61
+                'S' -> value += 67
+                'T' -> value += 71
+                'U' -> value += 73
+                'V' -> value += 79
+                'W' -> value += 83
+                'X' -> value += 89
+                'Y' -> value += 97
+                'Z' -> value += 101
+            }
+        }
+        return value
     }
     fun GetAnagrams(word: String) {
         WordsList.forEach{
